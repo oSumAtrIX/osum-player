7c1e1c537ee882bb2b6e06430459eeb13857be16
oSumAtrIX <johan.melkonyan1@web.de>
2023-03-16 10:05:35 +0100

    fix: console output when starting listening

--- server/src/index.js
+++ server/src/index.js
@@ -2,5 +2,5 @@ import app from "./app.js";
 
 const port = process.env.SERVER_PORT;
 app.listen(port, () => {
-	console.log(`Listening: http://${process.env.SERVER_PORT}:${port}`);
+	console.log(`Listening: http://localhost:${port}`);
 });
